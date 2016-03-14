57723c1f0fa375adadae680ea79b8ef2b0a372b4
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-03-14 14:47:59 +0000

    Modified

---
+++ servers/server.js
@@ -0,0 +1,25 @@
+// See also https://github.com/neumino/rethinkdbdash
+
+var rethinkdb = require('rethinkdbdash')({
+  pool: false,
+  cursor: true,
+  servers: [{host: 'localhost', port: 8080}] // Use the values of your running RethinkDB instance
+}); // You need to execute the module when you import it.
+// With the official driver:
+// var r = require('rethinkdb');
+
+//query.run().then(function(result) {
+//  console.log(result);
+//});
+
+rethinkdb.db('test').table('data').run().then(function(error, result) {
+	console.log(result);
+});
+
+
+/*
+rethinkdb.db('test').tableCreate('authors').run().then(function(err, result) {
+    if (err) throw err;
+    console.log(JSON.stringify(result, null, 2));
+});
+*/
